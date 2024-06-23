3eb36abfb8f3b77e09a4ddd3de7626d37778ebf1
KanybekovMaksat <maksatkanybekov2004@gmail.com>
2024-06-23 13:30:45 +0600

    second

--- server.js
+++ server.js
@@ -3,31 +3,34 @@ const app = express();
 const port = process.env.PORT || 5000;
 const path = require('path');
 const fs = require('fs');
+const axios = require('axios');
 
 app.use(express.static(path.resolve(__dirname, './dist')));
 
-// Function to set Open Graph metadata
 function setOpenGraphMetadata(data, title, description, image) {
-  data = data.replace(/\$OG_TITLE/g, title);
-  data = data.replace(/\$OG_DESCRIPTION/g, description);
+  data = data.replace(/\Makalabox/g, title);
+  data = data.replace(
+    /\$MakalaBox - платформа для обмена знаниями между преподавателями и студентами колледжей. Здесь они могут публиковать статьи по различным темам./g,
+    description
+  );
   data = data.replace(/\$OG_IMAGE/g, image);
   return data;
 }
 
-// Handle specific page routes for metadata
-app.get(['/', '/about', '/feed', '/article/*'], function(request, response) {
+app.get(['/', '/about', '/feed', '/article/*'], function (request, response) {
   const filePath = path.resolve(__dirname, './dist', 'index.html');
   const url = request.path;
 
-  fs.readFile(filePath, 'utf8', function(err, data) {
+  fs.readFile(filePath, 'utf8', async function (err, data) {
     if (err) {
       console.log(err);
       response.status(500).send('Internal Server Error');
       return;
     }
 
-    let title = 'Default Title';
-    let description = 'Default description';
+    let title = 'Makalabox';
+    let description =
+      'MakalaBox - платформа для обмена знаниями между преподавателями и студентами колледжей. Здесь они могут публиковать статьи по различным темам.';
     let image = 'https://i.imgur.com/V7irMl8.png';
 
     if (url === '/') {
@@ -45,18 +48,57 @@ app.get(['/', '/about', '/feed', '/article/*'], function(request, response) {
     } else if (url.startsWith('/article/')) {
       const articleId = url.split('/article/')[1];
       console.log(`Article page visited! ID: ${articleId}`);
-      title = `Article ${articleId}`;
-      description = `Description for article ${articleId}`;
-      image = 'https://api.makalabox.com/media/article/photos/2024/06/21/5926a7cc-f8c5-4f7a-91f8-508b4ccc51e6.png';
+
+      try {
+        const articleResponse = await axios.get(
+          `https://api.makalabox.com/api/articles/${articleId}`
+        );
+        const articleData = articleResponse.data.data;
+        title = articleData.title;
+        description = articleData.subtitle;
+        image = articleData.photo;
+        jsonLd = JSON.stringify({
+          '@context': 'https://schema.org',
+          '@type': 'Article',
+          mainEntityOfPage: {
+            '@type': 'WebPage',
+            '@id': `https://makalabox.com/article/${articleId}/`,
+          },
+          headline: articleData.title,
+          description: articleData.subtitle,
+          image: {
+            '@type': 'ImageObject',
+            url: articleData.photo,
+            width: 200,
+            height: 98,
+          },
+          author: {
+            '@type': 'Person',
+            name: articleData.author.fullName,
+          },
+          publisher: {
+            '@type': 'Organization',
+            name: articleData.author.fullName,
+            logo: {
+              '@type': 'ImageObject',
+              url: '',
+              width: '',
+              height: '',
+            },
+          },
+          datePublished: articleData.created,
+        });
+      } catch (error) {
+        console.log(`Error fetching article ${articleId}:`, error);
+      }
     }
 
-    data = setOpenGraphMetadata(data, title, description, image);
+    data = setMetadata(data, title, description, image, jsonLd);
     response.send(data);
   });
 });
 
-// Handle all other routes
-app.get('*', function(request, response) {
+app.get('*', function (request, response) {
   const filePath = path.resolve(__dirname, './dist', 'index.html');
   response.sendFile(filePath);
 });
